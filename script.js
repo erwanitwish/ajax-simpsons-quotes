5eae89a8fe8ee806e17339e5540833562161f91b
erwanitwish <quillec.erwan@gmail.com>
2020-04-08 14:42:23 +0200

    done

---
+++ script.js
@@ -0,0 +1,23 @@
+// This function loads pokemon data from the Pokemon API
+function fetchPokemonJSON() {
+  // Feel free to download this HTML and edit it, to use another Pokemon ID
+  const pokemonId = 1;
+  const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+  axios.get(url)
+    .then(function(response) {
+      return response.data[0]; // response.data instead of response.json() with fetch
+    })
+    .then(function(quote) {
+      console.log('data decoded from JSON:', quote);
+
+      // Build a block of HTML
+      const quoteHtml = `
+        <p><strong>${quote.character}</strong></p>
+        <img src="${quote.image}" />
+        <p>${quote.quote}</p>
+      `;
+      document.querySelector('#quote-part').innerHTML = quoteHtml;
+    });
+}
+
+fetchPokemonJSON();
